70b6272e08a8ffc901f2a84012f3291460efb6d2
David Stamm <david@paperlesspost.com>
2012-12-31 09:28:32 -0500

    Better gunstage. Git functions cleanup.

--- git/functions.sh
+++ git/functions.sh
@@ -1,27 +1,34 @@
-function git_current_branch() {
+function git_branch() {
   ref=$(git symbolic-ref HEAD 2>/dev/null)
   echo ${ref#refs/heads/}
 }
 
 function git_prompt() {
-  if [ -z $(git_current_branch) ]; then
+  branch="$(git_branch)"
+  if [ -z $branch ]; then
     echo ""
   else
-    echo " [$(git_current_branch)]"
+    echo " [$branch]"
   fi
 }
 
-function gs_nth() {
+function git_status_nth() {
   n=$(($1 + 1)) # ignore the 1st line of git status output (the current branch)
   git status -sb | strp | nthline $n | nthword 2 | chomp
 }
 
-# ga [M=modified] [D=deleted] [?=new] [1=1st file]
+
+
+# takes multiple arguments, any of which can be
+#   1-9 : nth file in git status listing
+#   M   : all modified files
+#   D   : all deleted files
+#   ?   : all untracked files
 function ga() {
   for arg in $@; do
-    if [[ $arg =~ ^[0-9]+$ ]] ; then
-      gs_nth $arg | xargs git add
-    elif [[ $arg =~ ^[MD?]$ ]] ; then
+    if [[ $arg =~ ^[0-9]+$ ]]; then
+      git_status_nth $arg | xargs git add
+    elif [[ $arg =~ ^[MD?]$ ]]; then
       git status -sb | strp | grep "^$arg" | nthword 2 | inline | xargs git add
     else
       git add $arg
@@ -31,13 +38,13 @@ function ga() {
 
 # gcp 2 -> copies to clipboard the 2nd filename listed by git status
 function gcp() {
-  gs_nth $1 | pbcopy
+  git_status_nth $1 | pbcopy
 }
 
 # gd 2 -> shows diff for 2nd file listed by git status
 function gd() {
-  if [[ $1 =~ ^[0-9]+$ ]] ; then
-    gs_nth $1 | xargs git diff
+  if [[ $1 =~ ^[0-9]+$ ]]; then
+    git_status_nth $1 | xargs git diff
   else
     git diff $1
   fi
@@ -45,14 +52,13 @@ function gd() {
 
 function gpu() {
   if [ -z $1 ]; then
-    git push -u origin $(git_current_branch)
+    git push -u origin $(git_branch)
   else
     git push -u origin $1
   fi
 }
 
-# git remove remotes (except origin and root)
-# makes everything faster
+# git remove remotes! (except origin and root)
 function grr!() {
   remotes=`git remote -v | cut -f 1 | uniq | grep -vE '(origin|root)'`
   for remote in $remotes; do
@@ -60,3 +66,12 @@ function grr!() {
     echo "removed $remote"
   done
 }
+
+function gunstage() {
+  if [[ $1 =~ ^[0-9]+$ ]]; then
+    git_status_nth $1 | xargs git reset HEAD
+  else
+    git reset HEAD
+  fi
+}
+
