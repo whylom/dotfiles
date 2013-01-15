8c24028e8fbd54aa4bb268bef316b81df1f038c1
David Stamm <david@paperlesspost.com>
2013-01-15 10:39:33 -0500

    Better YouTube running time logic.

--- dotjs/youtube.com.js
+++ dotjs/youtube.com.js
@@ -13,6 +13,25 @@ $('#watch-headline h1').css('font-size', '13pt');
 $('#watch7-content').css('width', '854px');
 
 // Get video's running time from markup, and insert into document title.
-var runningTime = $('meta[itemprop="duration"]').attr('content');
-runningTime = '(' + runningTime.replace('PT', '').replace('M', ':').replace('S', '') + ')';
-document.title = document.title.replace('- YouTube', runningTime);
+var duration = $('meta[itemprop="duration"]').attr('content');
+var parts = duration.replace(/(PT|S)/g, '').split('M');
+var min = parseInt(parts[0]);
+var sec = parts[1];
+
+// split 72 minutes into 1 hour, 12 minutes
+if (min > 60) {
+  var hrs = Math.round(min / 60);
+  min = (min - (hrs * 60)).toString();
+  if (min.length == 1) min = min + '0';
+}
+
+// display 2 seconds as "02"
+if (sec.length == 1) sec = '0' + sec;
+
+if (hrs) {
+  var time = [hrs, min, sec].join(":");
+} else {
+  var time = [min, sec].join(":");
+}
+
+document.title = document.title.replace('- YouTube', '(' + time + ')');
