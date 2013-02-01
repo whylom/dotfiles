39e440822ffc60ddc65b45cb29a9142483923f17
David Stamm <david@paperlesspost.com>
2013-02-01 10:34:22 -0500

    Add numbering to gs.

--- git/functions.sh
+++ git/functions.sh
@@ -15,14 +15,15 @@ function git_prompt() {
 function git_log_nth() {
   git log --pretty=format:'%H' | nthline $1
 }
+
 function git_status_nth() {
   git status -sb | strp | nthline $(($1 + 1)) | nthword 2 | chomp
 }
 
 function run() {
   echo -e "\033[36m$1\033[0m" # warn user what we're about to do
-  sleep 1.5            # give them some time to Ctrl-C
-  eval $1              # FIRE!
+  sleep 1.5                   # give them some time to Ctrl-C
+  eval $1                     # FIRE!
 }
 
 # takes multiple arguments, any of which can be
@@ -91,6 +92,23 @@ function grr!() {
     git remote rm $remote
     echo "removed $remote"
   done
+}
+
+function gs() {
+  OIFS="${IFS}"
+  IFS=$'\n'
+  status=`git status -sb`
+  i=0
+
+  for line in $status; do
+    if [[ $i > 0 ]]; then
+      line="$i $line"
+    fi
+    echo "$line"
+    let i++
+  done
+
+  IFS="${OIFS}"
 }
 
 # copies to clipboard the SHA of the nth commit listed by git log
