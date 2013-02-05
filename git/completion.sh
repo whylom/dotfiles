ed3d3af111682e1b1cec6ca212278d879cdd77f2
David Stamm <david@paperlesspost.com>
2013-02-05 10:12:03 -0500

    autocomplete gr with remote names

--- git/completion.sh
+++ git/completion.sh
@@ -6,6 +6,14 @@ __branches() {
     return 0
 }
 
+__remotes() {
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+    remotes=$(git remote | inline)
+    COMPREPLY=($(compgen -W "${remotes}" -- ${cur}))
+    return 0
+}
+
 complete -o default -o nospace -F __branches gbd
 complete -o default -o nospace -F __branches gco
 complete -o default -o nospace -F __branches gcob
@@ -15,3 +23,5 @@ complete -o default -o nospace -F __branches gmr
 complete -o default -o nospace -F __branches gp
 complete -o default -o nospace -F __branches gpr
 complete -o default -o nospace -F __branches gpu
+
+complete -o default -o nospace -F __remotes gf
