15df648a718aafac390565c40e4ac2e6c2b528c5
David Stamm <david@paperlesspost.com>
2013-02-08 14:45:32 -0500

    Remove annoying content.

--- dotjs/ted.com.js
+++ dotjs/ted.com.js
@@ -5,3 +5,8 @@ var minutes = Math.floor(duration / 60);
 var seconds = duration % 60;
 var time = [minutes,seconds].join(':');
 document.title = document.title.replace('| Video on TED.com', '(' + time + ')');
+
+// Remove parts of the page.
+$('#conversations, #conversation, #share_and_save').remove();
+var relatedTalks = $('.contentPod').has('.relatedPlaylists, .relatedTalks');
+$('.contentPod').not(relatedTalks).remove();
