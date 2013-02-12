b5414af85d70d8998553e097024c10ab9513be2a
David Stamm <david@paperlesspost.com>
2013-02-12 14:20:33 -0500

    Fix running time logic.

--- dotjs/ted.com.js
+++ dotjs/ted.com.js
@@ -3,6 +3,7 @@ var duration = $('meta[property="video:duration"]').attr('content');
 duration = parseInt(duration);
 var minutes = Math.floor(duration / 60);
 var seconds = duration % 60;
+if (seconds < 10) seconds = '0' + seconds;
 var time = [minutes,seconds].join(':');
 document.title = document.title.replace('| Video on TED.com', '(' + time + ')');
 
