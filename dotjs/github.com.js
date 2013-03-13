d11a56e65e7502ba95d29bd1b973771f4af985de
David Stamm <david@paperlesspost.com>
2013-03-13 09:35:13 -0400

    Restore original size of pull request diff code.

--- dotjs/github.com.js
+++ dotjs/github.com.js
@@ -1,11 +1,11 @@
 // Customize page title for issues to start with issue number
 document.title = document.title.replace(/(.*) · Issue #([0-9]+).*$/, '#$2 - $1');
 
-var box = $('.notification-indicator');
-var dot = box.find('.mail-status');
+// GitHub recently jacked up the size of code displayed in the "changes"
+// section of pull requests. Let's change it back, shall we?
+$('#files .file .data .diff-line').css('font-size', '12px');
 
 // Screen-scrape GitHub notifications page to find # of unread notifications.
-// Update notifications indicator with the current state.
 function getUnreadCount(callback) {
   $.get('/notifications', function(response) {
     var unread = $(response).find('.filter-item:first .count').text();
@@ -13,7 +13,10 @@ function getUnreadCount(callback) {
   });
 }
 
+// Update notifications indicator with the current state.
 function updateOnce() {
+  var box = $('.notification-indicator');
+  var dot = box.find('.mail-status');
   getUnreadCount(function(unread) {
     if (unread > 0) {
       box.addClass('contextually-unread');
