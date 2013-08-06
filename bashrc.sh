4dbdd87663a3d6060a56650feaa6b1943110ac11
David Stamm <stamm@generalassemb.ly>
2013-08-06 14:32:14 -0400

    Move display logic to fit function.
    With this commit, the time-consuming call to system_profiler is made
    only when the `fit` function is called, instead of every time a new
    shell is opened.

--- bashrc.sh
+++ bashrc.sh
@@ -2,11 +2,6 @@
 export EDITOR="subl -w"
 export PATH=/usr/local/bin:$PATH
 
-# save dimensions of main display (primarily for use in AppleScript)
-DISPLAY_DIMENSIONS=$(system_profiler SPDisplaysDataType | grep 'Resolution' | head -1 | tr -d ' ' | cut -f 2 -d ':')
-export DISPLAY_WIDTH=$(echo $DISPLAY_DIMENSIONS | cut -f 1 -d 'x' )
-export DISPLAY_HEIGHT=$(echo $DISPLAY_DIMENSIONS | cut -f 2 -d 'x' )
-
 # load .sh files
 for f in ~/.dotfiles/**/*.sh; do source $f; done
 
