d69c9d7f1f61094ad91f33dc1e1dbd05babaafc1
David Stamm <stamm@generalassemb.ly>
2013-08-12 16:49:02 -0400

    Run multiple specs with t? function.

--- ruby/functions.sh
+++ ruby/functions.sh
@@ -3,35 +3,45 @@ function t?() {
   query="$(echo $@ | sed 's/ /.*/g')"
 
   # get paths to all relevant Ruby files in the spec directory
-  files=$(find spec -name *.rb | grep -vE '(factories|helper|support)')
+  specs=$(find spec -name *.rb | grep -vE '(factories|helper|support)')
 
-  # if file matches query, add it to results array
-  results=()
-  for file in $files; do
-    if [[ $file =~ $query ]]; then
-      results+=($file)
+  # add any spec that matches the query pattern to the results array
+  matches=()
+  for spec in $specs; do
+    if [[ $spec =~ $query ]]; then
+      matches+=($spec)
     fi
   done
-  num_results=${#results[@]}
+  num_matches=${#matches[@]}
 
-  if [[ $num_results = 0 ]]; then
+  if [[ $num_matches = 0 ]]; then
     # if 0 files were found, quit
     echo "No matches"
-  elif [[ $num_results = 1 ]]; then
+  elif [[ $num_matches = 1 ]]; then
     # if 1 file was found, go ahead & run it
-    run "rspec ${results[0]}"
+    run "spin push ${matches[0]}"
   else
-    # otherwise, display a numbered list of all results
-    for (( i=0; i<${num_results}; i++ )); do
-      echo "$(($i+1)). ${results[$i]}"
+    # otherwise, display a numbered list of all matches
+    for (( i=0; i<${num_matches}; i++ )); do
+      echo "$(($i+1)). ${matches[$i]}"
     done
 
     # let the user select a file by number
-    read -p "Which spec shall I run then? ('q' to quit): " num
-    if [[ $num = 'q' ]]; then return; fi
-    file=${results[$num-1]}
+    read -p "Select one or more specs by number (or 'q' to quit): " response
+    if [[ $response = 'q' ]]; then return; fi
 
-    # run the selected file
-    run "spin push $file"
+    # split user's string response to an array of indexes
+    IFS=' ' read -a indexes <<< "$response"
+
+    # use indexes to create a space-delimited list of files
+    specs=""
+    for (( i=0; i<${#indexes[@]}; i++ )); do
+      index=${indexes[$i]}
+      spec=${matches[$index-1]}
+      specs="$specs $spec"
+    done
+
+    # run the selected spec(s)
+    run "spin push $specs"
   fi
 }
