dc54d2ef4a0e09ada61f797aaa321ab4850bed27
David Stamm <stamm@generalassemb.ly>
2013-08-12 16:49:16 -0400

    Tweak Ruby/Rails aliases.

--- ruby/aliases.sh
+++ ruby/aliases.sh
@@ -1,9 +1,15 @@
 alias b="bundle exec"
 alias bi="bundle install"
 
-alias r="b rails"
-alias c="r console"
-alias s="r server"
+alias c="b rails console"
+alias r="b rake"
 
-alias rdbm="b rake db:migrate"
-alias rdbmt="b rake db:migrate RAILS_ENV=test"
+function rdbm() {
+  r db:migrate ${1:-development}
+}
+alias rdbm+="rdbm && rdbm test"
+
+alias rdbr="r db:drop db:setup"
+alias rdbr+="rdbr && rdbr RAILS_ENV=test"
+
+alias t="spin push"
