0e8fde7877a8d83308dab074b2f41e0cb11c7294
David Stamm <stamm@generalassemb.ly>
2013-08-14 09:36:12 -0400

    Refactor YouTube script to handle new annoyances.

--- dotjs/youtube.com.js
+++ dotjs/youtube.com.js
@@ -1,30 +1,40 @@
-// Hide comments, siderbar, and footer
-$('#comments-view').remove();
-$('.watch-sidebar-section').remove();
-$('#footer-container').remove();
+var times = 0;
 
-// Description is always expanded
-$('.yt-horizontal-rule').remove();
-$('#watch-description-toggle').remove();
-$('.yt-uix-expander-collapsed').removeClass('yt-uix-expander-collapsed');
-$('#watch-description-toggle').remove();
+(function() {
+  // Hide comments, siderbar, and footer.
+  $('#comments-view').remove();
+  $('.comments-pagination').remove();
+  $('.watch-sidebar-section').remove();
+  $('#footer-container').remove();
 
-// Get video's duration from meta tag data.
-var duration = $('meta[itemprop="duration"]').attr('content');
-var parts = duration.replace(/(PT|S)/g, '').split('M');
-var min = parseInt(parts[0]);
-var sec = parseInt(parts[1]);
+  // Expand description, hide controls to toggle description.
+  $('.yt-horizontal-rule').remove();
+  $('#watch-description-toggle').remove();
+  $('.yt-uix-expander-collapsed').removeClass('yt-uix-expander-collapsed');
+  $('#watch-description-toggle').remove();
 
-// split 72 minutes into 1 hour, 12 minutes
-if (min > 60) {
-  var hrs = Math.floor(min / 60);
-  min = (min - (hrs * 60)).toString();
-}
+  // Get video's duration from meta tag data.
+  var duration = $('meta[itemprop="duration"]').attr('content');
+  var parts = duration.replace(/(PT|S)/g, '').split('M');
+  var min = parseInt(parts[0]);
+  var sec = parseInt(parts[1]);
 
-// prepend hours & seconds with a zero as needed
-if (hrs && min < 10) min = '0' + min;
-if (sec < 10) sec = '0' + sec;
+  // Split 60+ minutes into hours and minutes.
+  if (min > 60) {
+    var hrs = Math.floor(min / 60);
+    min = (min - (hrs * 60)).toString();
+  }
 
-// Add running time to document title.
-var time = hrs ? [hrs, min, sec] : [min, sec];
-document.title = document.title.replace('- YouTube', '(' + time.join(":") + ')');
+  // Prepend hours & seconds with a zero as needed.
+  if (hrs && min < 10) min = '0' + min;
+  if (sec < 10) sec = '0' + sec;
+
+  // Add running time to document title.
+  var time = hrs ? [hrs, min, sec] : [min, sec];
+  document.title = document.title.replace('▶ ', '').replace('- YouTube', '(' + time.join(":") + ')');
+
+  // Repeat 10 times (once a second) to catch asynchronous changes to the page.
+  if (++times < 10) {
+    setTimeout(arguments.callee, 1000);
+  }
+})();
