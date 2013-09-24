0a4c2f3203c04f2bb13699f00957bfa3d5ea18a8
David Stamm <stamm@generalassemb.ly>
2013-09-24 11:57:32 -0400

    Add .js file to make HuffPo less godawful.

---
+++ dotjs/huffingtonpost.com.js
@@ -0,0 +1,7 @@
+$('img, #threeup_top_wrapper, .blog_banner_wrapper, .grid.third.flush_top').hide();
+
+$('#badges_v2_1').css('opacity', 0);
+
+setTimeout(function() {
+  $('.comments').hide();
+}, 1000);
