78b5be47a2a05c0c35a74e20c595ce1e5f385c8e
David Stamm <stamm@generalassemb.ly>
2013-10-16 16:25:25 -0400

    Update rvm config to fix warnings.

--- bashrc.sh
+++ bashrc.sh
@@ -1,3 +1,7 @@
+# environment variables
+export EDITOR="subl -w"
+export PATH=/usr/local/bin:$PATH
+
 # load .sh files
 for f in ~/.dotfiles/**/*.sh; do source $f; done
 
@@ -6,7 +10,3 @@ if [ -e ~/.dotfiles/private.sh ]; then source ~/.dotfiles/private.sh; fi
 
 # go to our favorite directory if we're in ~
 if [ $(pwd) = $HOME ]; then cd $FAVE; fi
-
-# environment variables
-export EDITOR="subl -w"
-export PATH=/usr/local/bin:$PATH
