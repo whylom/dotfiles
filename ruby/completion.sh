b5f8a176912fd5efe0a4ced08263ffb8d6c1012f
David Stamm <stamm@generalassemb.ly>
2014-07-05 18:09:02 -0400

    Add app names to Heroku completion.

--- ruby/completion.sh
+++ ruby/completion.sh
@@ -1,5 +1,15 @@
 __heroku() {
-  local list="addons apps auth config domains logs ps releases run sharing account certs drains fork git help keys labs maintenance members orgs pg pgbackups plugins regions stack status update version"
+  local index list cmd
+
+  index=${#COMP_WORDS[@]}
+  cmd="${COMP_WORDS[COMP_CWORD-1]}"
+
+  if [ $index -eq 2 ]; then
+    list="addons apps auth config domains logs ps releases run sharing account certs drains fork git help keys labs maintenance members orgs pg pgbackups plugins regions stack status update version"
+  elif [[ $cmd = "-a" ]]; then
+    list=$HEROKU_APPS # defined in private.sh
+  fi
+
   COMPREPLY=($(compgen -W "${list}" -- ${COMP_WORDS[COMP_CWORD]}))
 }
 
