0ab5f0b87a0872ad061eeb60536a3e5336bdd1e7
David Stamm <stamm@generalassemb.ly>
2014-08-18 12:38:39 -0400

    Update TED.com customizations for new design.

--- dotjs/ted.com.js
+++ dotjs/ted.com.js
@@ -5,16 +5,7 @@ var minutes = Math.floor(duration / 60);
 var seconds = duration % 60;
 if (seconds < 10) seconds = '0' + seconds;
 var time = [minutes,seconds].join(':');
-document.title = document.title.replace('| Video on TED.com', '(' + time + ')');
+document.title = document.title.replace('| Talk Video | TED.com', '(' + time + ')');
 
 // Remove parts of the page.
-$('#conversations, #conversation, #share_and_save').remove();
-var relatedTalks = $('.contentPod').has('.relatedPlaylists, .relatedTalks');
-$('.contentPod').not(relatedTalks).remove();
-
-// Show/hide right-rail content by clicking headers.
-$('.contentPod .header').toggle(function() {
-  $(this).siblings('.content').hide();
-}, function() {
-  $(this).siblings('.content').show();
-});
+$('.talk-section--borderless').parents('.area').remove();
