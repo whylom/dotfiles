89e4359a621e47434eb7f11ad22c996c87d1240c
David Stamm <stamm@generalassemb.ly>
2015-01-04 21:30:38 -0500

    Extract running time logic to a function.

--- dotjs/youtube.com.js
+++ dotjs/youtube.com.js
@@ -1,3 +1,24 @@
+function getRunningTime() {
+  // Get video's duration from meta tag data.
+  var duration = $('meta[itemprop="duration"]').attr('content');
+  var parts = duration.replace(/(PT|S)/g, '').split('M');
+  var min = parseInt(parts[0]);
+  var sec = parseInt(parts[1]);
+
+  // Split 60+ minutes into hours and minutes.
+  if (min > 60) {
+    var hrs = Math.floor(min / 60);
+    min = (min - (hrs * 60)).toString();
+  }
+
+  // Format time as either hh:mm:ss or mm:ss
+  if (hrs && min < 10) min = '0' + min;
+  if (sec < 10) sec = '0' + sec;
+  var time = hrs ? [hrs, min, sec] : [min, sec];
+
+  return time.join(":");
+}
+
 (function() {
   // Hide comments, sidebar, and footer.
   $('#watch-discussion').remove();
@@ -18,25 +39,7 @@
     'paddingLeft': '105px',
   });
 
-  // Get video's duration from meta tag data.
-  var duration = $('meta[itemprop="duration"]').attr('content');
-  var parts = duration.replace(/(PT|S)/g, '').split('M');
-  var min = parseInt(parts[0]);
-  var sec = parseInt(parts[1]);
-
-  // Split 60+ minutes into hours and minutes.
-  if (min > 60) {
-    var hrs = Math.floor(min / 60);
-    min = (min - (hrs * 60)).toString();
-  }
-
-  // Prepend hours & seconds with a zero as needed.
-  if (hrs && min < 10) min = '0' + min;
-  if (sec < 10) sec = '0' + sec;
-
-  // Add running time to document title.
-  var time = hrs ? [hrs, min, sec] : [min, sec];
-  document.title = document.title.replace('▶ ', '').replace('- YouTube', '(' + time.join(":") + ')');
+  document.title = document.title.replace('▶ ', '').replace('- YouTube', '(' + getRunningTime() + ')');
 
   // Keep repeating all of the above to catch asynchronous changes to the page.
   setTimeout(arguments.callee, 1000);
