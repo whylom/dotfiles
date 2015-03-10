21088aa9f91040042e1ab5cd62e595230b315470
David Stamm <stamm@generalassemb.ly>
2015-03-09 23:07:53 -0400

    Refactor Trello hack to work with live updating.

--- dotjs/trello.com.js
+++ dotjs/trello.com.js
@@ -1,23 +1,31 @@
-(function() {
-  // wait until UI is populated with data
-  if ($('.list-card').length == 0) {
-    return setTimeout(arguments.callee, 10);
-  }
+var css = '\
+  .list-header .card-count { \
+    position: absolute;      \
+    right: 21px;             \
+    top: 3px;                \
+    color: #999;             \
+    font-size: 12px;         \
+  }                          \
+';
 
+var style = $('<style></style>').html(css);
+$('head').append(style);
+
+(function() {
   // display the # of cards in each list's header
   $('.list').each(function() {
     var header   = $('.list-header h2', this);
     var numCards = $('.list-card', this).length;
-    var count    = $('<span>'+ numCards +'</span>');
+    var cardCount = header.find('.card-count');
 
-    count.css({
-      'position': 'absolute',
-      'right': '21px',
-      'top': '3px',
-      'color': '#999',
-      'font-size': '12px'
-    });
-
-    header.prepend(count);
+    if (cardCount.length) {
+      cardCount.html(numCards);
+    } else {
+      header.prepend(
+        $('<span class="card-count">'+ numCards +'</span>')
+      );
+    }
   })
+
+  setTimeout(arguments.callee, 1000);
 })();
