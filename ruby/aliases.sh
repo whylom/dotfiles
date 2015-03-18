24a531fa448f306bad715ef776e018c4d7344cbb
David Stamm <stamm@generalassemb.ly>
2015-03-18 14:43:05 -0400

    Make c! a fn with completion and what-not.

--- ruby/aliases.sh
+++ ruby/aliases.sh
@@ -8,9 +8,11 @@ alias rs="bundle exec rspec"
 function rdbm() {
   run "bundle exec rake db:migrate RAILS_ENV=${1:-development}"
 }
-alias rdbm+="rdbm && rdbm test"
 
 alias h="heroku"
 alias h?="heroku help"
-alias c!="run 'heroku run console -r production'"
 alias db!="run 'heroku pg:psql -r production'"
+
+function c!() {
+  run "heroku run console -r ${1:-production}"
+}
--- ruby/completion.sh
+++ ruby/completion.sh
@@ -5,7 +5,7 @@ __heroku() {
   cmd="${COMP_WORDS[COMP_CWORD-1]}"
 
   if [ $index -eq 2 ]; then
-    list="addons apps auth authorizations config domains logs ps releases run sharing account certs drains fork git help keys labs maintenance members orgs pg pgbackups plugins regions stack status update version"
+    list="addons apps auth:login auth:logout auth:token auth:whoami authorizations config domains logs ps releases run sharing account certs drains fork git help keys labs maintenance members orgs pg pgbackups plugins regions stack status update version"
   elif [[ $cmd = "help" ]]; then
     list="addons apps auth authorizations config domains logs ps releases run sharing account certs drains fork git help keys labs maintenance members orgs pg pgbackups plugins regions stack status update version"
   elif [[ $cmd = "run" ]]; then
@@ -29,8 +29,14 @@ __rbenv() {
   COMPREPLY=($(compgen -W "${list}" -- ${COMP_WORDS[COMP_CWORD]}))
 }
 
+__envs() {
+  local list="staging production"
+  COMPREPLY=($(compgen -W "${list}" -- ${COMP_WORDS[COMP_CWORD]}))
+}
+
 complete -F __bundle bundle
 complete -F __heroku heroku
 complete -F __heroku h
 complete -F __heroku h?
+complete -F __envs c!
 complete -F __rbenv rbenv
