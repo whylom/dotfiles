4b5d4894d4952a136f202cf969216a4a4221321a
David Stamm <stamm@generalassemb.ly>
2016-05-19 16:22:14 -0400

    Completion for Homebrew

--- bash/completion.sh
+++ bash/completion.sh
@@ -4,3 +4,23 @@ __ssh_hosts() {
 }
 
 complete -F __ssh_hosts ssh
+
+
+__homebrew_apps=$(brew list)
+__homebrew() {
+  local index cmd commands list
+
+  index=${#COMP_WORDS[@]}
+  cmd="${COMP_WORDS[COMP_CWORD-1]}"
+  commands="create doctor edit home info install list options pin search uninstall unpin update upgrade"
+
+  if [ $index -eq 2 ]; then
+    list=$commands
+  elif [ $index -eq 3 ]; then
+    list=$(brew list)
+  fi
+
+  COMPREPLY=($(compgen -W "${list}" -- ${COMP_WORDS[COMP_CWORD]}))
+}
+
+complete -F __homebrew brew
