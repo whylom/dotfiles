96fb10d831fd9e5e7e35c9243494c753eea009f4
David Stamm <david.stamm@spreemo.com>
2016-09-02 19:02:28 -0400

    Tweak install script

--- install.sh
+++ install.sh
@@ -3,10 +3,21 @@ function symlink() {
   ln -s $1 $2
 }
 
+function copy_if_needed() {
+  if [ ! -f $2 ]; then cp $1 $2; fi
+}
+
 # symlink all dotfiles ending in .symlink
 for original in $HOME/.dotfiles/*/**.symlink; do
-  link=$(basename $original) # extract filename
+  link=$(basename $original) # extract filename from path
   link=${link%.symlink}      # remove ".symlink"
   link="$HOME/.$link"        # prepend with "~/."
   symlink $original $link
 done
+
+# explictly symlink folders
+symlink .js ~/.js
+
+# make sure .bash_profile and .bashrc are in place
+copy_if_needed bash/.bash_profile ~/.bash_profile
+copy_if_needed bash/.bashrc       ~/.bashrc
