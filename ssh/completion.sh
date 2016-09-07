76c594d50b911b0dd0d13d3e4421bca2757fa555
David Stamm <david.stamm@spreemo.com>
2016-09-07 14:12:43 -0400

    Configure SSH keys. Move prompts out of bin/setup

---
+++ ssh/completion.sh
@@ -0,0 +1,6 @@
+__ssh_hosts() {
+  local list=$(grep "Host " ~/.ssh/config | grep -v "*" | sed 's/Host //g')
+  COMPREPLY=($(compgen -W "${list}" -- ${COMP_WORDS[COMP_CWORD]}))
+}
+
+complete -F __ssh_hosts ssh
