616a871d2a1135079352bfd88aba1ca0ddb7135c
David Stamm <david.stamm@spreemo.com>
2016-09-17 10:46:50 -0400

    Update TED.com dotjs

--- .js/ted.com.js
+++ .js/ted.com.js
@@ -1,11 +1,14 @@
-// Get video's running time from markup, and insert into document title.
+// Get video's running time and format as MM:SS
 var duration = $('meta[property="video:duration"]').attr('content');
 duration = parseInt(duration);
 var minutes = Math.floor(duration / 60);
 var seconds = duration % 60;
 if (seconds < 10) seconds = '0' + seconds;
 var time = [minutes,seconds].join(':');
-document.title = document.title.replace('| Talk Video | TED.com', '(' + time + ')');
+
+// Remove "TED.com" boilerplate from document title and append running time.
+var title = document.title.split('|')[0];
+document.title = title + '(' + time + ')';
 
 // Remove parts of the page.
 $('.talk-section--borderless').parents('.area').remove();
