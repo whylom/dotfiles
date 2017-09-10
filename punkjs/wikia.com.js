57629c210c0383ec632733b1f212fa73854fa273
David Stamm <david.stamm@youearnedit.com>
2017-09-09 22:08:48 -0400

    Tweak the wikia.com tweaks

--- punkjs/wikia.com.js
+++ punkjs/wikia.com.js
@@ -1,3 +1,6 @@
-$('#globalNavigation').remove();
-$('#WikiaRailWrapper').remove();
+// Hide header (page looks slightly better without it)
+$('.wds-global-navigation-wrapper').remove();
+
+// Hide grotesque geek culture clickbait
+$('#WikiaRailWrapper').remove(); // sidebar
 $('footer').remove();
