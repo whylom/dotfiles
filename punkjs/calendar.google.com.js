8942ad6c5d6d6a7d2033e4f26d8d50daeb9cbf2f
David Stamm <david.stamm@youearnedit.com>
2017-12-28 14:32:28 -0500

    Make hangouts link easier to see in Google Calendar

--- punkjs/calendar.google.com.js
+++ punkjs/calendar.google.com.js
@@ -10,3 +10,11 @@ $(document).keydown((event) => {
     }
   }
 });
+
+(function() {
+  // Make the hangouts link easier to see
+  $('a:contains("Join meeting")').css('fontWeight', 'bold');
+
+  // The above elements are not in the page on load, so keep polling
+  setTimeout(arguments.callee, 500);
+})();
