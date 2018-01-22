fe3e59c317d91230ea11705a0e84e68f3041a201
David Stamm <david.stamm@youearnedit.com>
2018-01-22 09:07:49 -0500

    Show masthead on section pages

--- punkjs/nytimes.com.js
+++ punkjs/nytimes.com.js
@@ -2,7 +2,11 @@ function isFrontPage() {
   return document.location.pathname == '/';
 }
 
-if (!isFrontPage()) {
+function isSectionPage() {
+  return document.location.pathname.indexOf('/section') == 0;
+}
+
+if (!isFrontPage() && !isSectionPage()) {
   $('#masthead, #ribbon, .newsletter-signup, .supplemental, .story-meta-footer-sharetools, .comments-button').remove()
   $('.navigation-edge').hide();
   $('main').css('borderTop', 'none');
