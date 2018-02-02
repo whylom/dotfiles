70dda6e7b8571cefc5604cddc22fc8c3965e76a2
David Stamm <david.stamm@youearnedit.com>
2018-02-02 14:39:50 -0500

    Remove temporary aliases

--- heroku/aliases.sh
+++ heroku/aliases.sh
@@ -10,10 +10,3 @@ alias hcp?="heroku config -r production | grep -i"
 alias hcs?="heroku config -r staging | grep -i"
 alias hcp!="heroku config:set -r production"
 alias hcs!="heroku config:set -r staging"
-
-# Temporary overrides
-alias h+="run 'git push heroku master'"
-alias hl="run 'heroku logs --tail'"
-alias c!="run 'heroku run racksh'"
-alias db!="run 'heroku pg:psql'"
-alias rdbm!="run 'heroku run rake db:migrate'"
