19bf1cb5abaf7f4f2eac2419c2701ecbd400446d
David Stamm <david.stamm@logikcull.com>
2018-05-16 09:03:22 -0400

    Update Vagrant aliases

--- vagrant/aliases.sh
+++ vagrant/aliases.sh
@@ -1,5 +1,4 @@
 alias v="vagrant"
-alias vp="v provision"
-alias vs="ssh vagrant"
-alias vu="v up"
-alias vx="ssh vagrant"
+alias v?="vagrant status"
+alias vssh="ssh vagrant"
+alias vup="vagrant up"
