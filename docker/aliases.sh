69e81e4f9fb1415e237717fc4ca0801f62ef27c1
David Stamm <david.stamm@logikcull.com>
2019-03-25 10:49:34 -0400

    Latest changes

---
+++ docker/aliases.sh
@@ -0,0 +1,15 @@
+alias d="docker"
+
+alias dc="docker-compose"
+alias dc+="docker-compose up -d"
+alias dc-="docker-compose down"
+alias dc~="docker-compose restart"
+alias dcx="docker-compose exec app"
+alias dbash="docker-compose exec app bash"
+alias dlog="docker-compose logs -f"
+alias dlogs="dlog"
+
+alias ds="docker-sync"
+alias ds+="docker-sync start"
+alias ds-="docker-sync stop"
+alias ds~="docker-sync stop && docker-sync start"
---
+++ punkjs/epicurious.com.js
@@ -0,0 +1,4 @@
+(function() {
+  $('aside, .cne-interlude-container, .video-container').remove();
+  setTimeout(arguments.callee, 100);
+})();
