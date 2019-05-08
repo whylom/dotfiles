91ce7bb245be0a77b26fbc9de939c08e9748b505
David Stamm <david.stamm@logikcull.com>
2019-05-08 19:11:33 -0400

    Leave me alone, Gamepedia

--- punkjs/gamepedia.com.js
+++ punkjs/gamepedia.com.js
@@ -1,1 +1,9 @@
-$('#-pxqciy').remove();
+$('#-pxqciy, #-pngsyo, #-pfvqrenvy_zvarpensg_tnzrcrqvn').remove();
+
+$('#bodyContent').css('width', '75%');
+
+$('table.msgbox').css({
+  'backgroundColor': 'lightgray',
+  'border': 'none',
+  'opacity': 0.4
+});
