92a5e27eff07bedb4cd7f96a1f3ab2705fe6e2b5
David Stamm <david.stamm@logikcull.com>
2019-05-09 09:05:08 -0400

    Update Docker aliases

--- docker/aliases.sh
+++ docker/aliases.sh
@@ -1,15 +1,20 @@
 alias d="docker"
 
 alias dc="docker-compose"
-alias dc+="docker-compose up -d"
-alias dc-="docker-compose down"
+
+alias dc+="docker-compose start"
+alias dc-="docker-compose stop"
 alias dc~="docker-compose restart"
+
+alias dc++="docker-compose up -d"
+alias dc--="docker-compose down"
+
 alias dcx="docker-compose exec app"
 alias dbash="docker-compose exec app bash"
 alias dlog="docker-compose logs -f"
-alias dlogs="dlog"
+alias dprune="docker container prune -f && docker volume prune -f"
 
 alias ds="docker-sync"
 alias ds+="docker-sync start"
 alias ds-="docker-sync stop"
-alias ds~="docker-sync stop && docker-sync start"
+alias ds~="docker-sync stop > /dev/null && docker-sync start > /dev/null"
