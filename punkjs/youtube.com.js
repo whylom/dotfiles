646ad516fee433953d5632b2ebee342509e52cbb
David Stamm <david.stamm@logikcull.com>
2019-11-05 15:10:07 -0500

    Update & upgrade YouTube customizations

--- punkjs/youtube.com.js
+++ punkjs/youtube.com.js
@@ -1,14 +1,15 @@
 function duration() {
-  var wtf = $('#player script:last').html();
-  var matches = wtf.match(/"length_seconds":"(\d+)"/)
-  return parseInt(matches[1]) - 1;
+  var wtf = $('script:contains("approxDurationMs")').html();
+  var durationInMs = wtf.match(/\\"approxDurationMs\\":\\"(\d+)\\"/)[1];
+  var durationInSeconds = parseInt(durationInMs) / 1000
+  return durationInSeconds;
 }
 
 function format(duration) {
   // Calculate hours, minutes, and seconds from total duration.
   var hours = Math.floor(duration / 3600);
   var minutes = Math.floor((duration - (hours * 3600)) / 60);
-  var seconds = duration - (hours * 3600) - (minutes * 60);
+  var seconds = Math.floor(duration - (hours * 3600) - (minutes * 60));
 
   // Format running time as "1:02:03" or "2:03" or "0:03"
   if (hours && minutes < 10) minutes = "0" + minutes;
@@ -25,20 +26,16 @@ function putDurationInTitle() {
 }
 
 function author() {
-  if (isVideo()) {
-    return $('#owner-container a').html();
-  } else {
-    return $('#owner-name a').html();
-  }
+  return $('.ytd-channel-name a:first').text();
 }
 
-function titleContainsAuthor() {
-  return document.title.indexOf(author()) > -1;
+function titleStartsWithAuthor() {
+  return document.title.indexOf(author()) == 0;
 }
 
 function putAuthorInTitle() {
-  if (!titleContainsAuthor()) {
-    document.title = author() + ' - ' + document.title.replace('- YouTube', '');
+  if (!titleStartsWithAuthor()) {
+    document.title = author() + ' - ' + document.title;
   }
 }
 
@@ -51,12 +48,21 @@ function isPlaylist() {
   return url.pathname == '/playlist' && url.search.indexOf('list=WL') == -1;
 }
 
+function removeUnreadCount() {
+  document.title = document.title.replace(/ \(\d+\) /, ' ');
+}
+
+function removeEmoji() {
+  var emoji = /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|[\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|[\ud83c[\ude32-\ude3a]|[\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g
+  document.title = document.title.replace(emoji, '');
+}
+
 (function() {
-  if (isVideo()) {
-    putDurationInTitle();
-    putAuthorInTitle();
-  } else if (isPlaylist()) {
-    putAuthorInTitle();
-  }
+  if (isVideo()) putDurationInTitle();
+
+  putAuthorInTitle();
+  removeUnreadCount();
+  removeEmoji();
+
   setTimeout(arguments.callee, 500);
 })();
