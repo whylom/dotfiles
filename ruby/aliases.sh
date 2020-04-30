e05c41dec54d94a204195e84ec81db2d26c42672
David Stamm <david.stamm@logikcull.com>
2020-04-30 16:52:18 -0400

    Alias for running Rails daemonized

--- ruby/aliases.sh
+++ ruby/aliases.sh
@@ -10,7 +10,8 @@ alias r="bundle exec rake"
 alias routes?="bundle exec rake routes | grep"
 
 alias r-="cat tmp/pids/server.pid | xargs kill -9"
-alias r!="run 'bundle exec rails server -p 3001'"
+alias r!="bundle exec rails server -p 3001"
+alias rd!="bundle exec rails server -p 3001 -d"
 
 alias rdbm="run 'bundle exec rake db:migrate'"
 alias rdbm~="run 'bundle exec rake db:migrate:redo'"
