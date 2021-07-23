9d9789bd4440a38c8ff136fa4fb52a43094c4285
David Stamm <david.stamm@logikcull.com>
2021-07-23 14:25:34 -0400

    Hide Fandom footer full of clickbait

--- punkjs/fandom.com.js
+++ punkjs/fandom.com.js
@@ -1,10 +1,1 @@
-
-$('aside.page__right-rail, #top_leaderboard, #WikiaBarWrapper, .global-navigation__links').remove();
-
-// This is not working for some reason
-// $(document).on('keydown', (e) => {
-//   let backslash = 191;
-//   if (e.keyCode == backslash) {
-//     $('a.global-navigation__search svg').click()
-//   }
-// });
+$('aside.page__right-rail, #top_leaderboard, #WikiaBarWrapper, .global-navigation__links, #mixed-content-footer').remove();
