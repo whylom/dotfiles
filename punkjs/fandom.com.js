d8c5c62809d46f58a7e6fe19a7e2df9855e45080
David Stamm <david.stamm@logikcull.com>
2021-09-07 10:38:10 -0400

    Resolved merge conflict

--- punkjs/fandom.com.js
+++ punkjs/fandom.com.js
@@ -1,1 +1,4 @@
-$('aside.page__right-rail, #top_leaderboard, #WikiaBarWrapper, .global-navigation__links, #mixed-content-footer').remove();
+$('.top-ads-container, aside.page__right-rail, #top_leaderboard, #WikiaBarWrapper, .global-navigation__links, #mixed-content-footer').remove();
+
+$('.global-navigation, .global-navigation__top, .global-navigation__bottom').css('backgroundColor', 'lightgray');
+$('.global-navigation__icon').css('border', 'none');
